56c1ef093e9484e9a1074dee8199128efcd3c5e3
Karolina Kelkel <ms.karolina.kelkel@gmail.com>
2023-04-03 00:37:19 +0200

    Rename variables

--- Statistics.py
+++ Statistics.py
@@ -35,7 +35,7 @@ class DataSetWithStatistics(DataSet):
         super().__init__(data=data_set)
 
         self.__sum_of_observations = None
-        self.__is_number_of_items_even = None
+        self.__is_num_of_items_even = None
 
     def sum_of_observations(self) -> [int, float]:
         if self.__sum_of_observations is None:
@@ -43,11 +43,11 @@ class DataSetWithStatistics(DataSet):
 
         return self.__sum_of_observations
 
-    def is_number_of_items_even(self) -> bool:
-        if self.__is_number_of_items_even is None:
-            self.__is_number_of_items_even = self.size % 2 == 0
+    def is_num_of_items_even(self) -> bool:
+        if self.__is_num_of_items_even is None:
+            self.__is_num_of_items_even = self.size % 2 == 0
 
-        return self.__is_number_of_items_even
+        return self.__is_num_of_items_even
 
     def arithmetic_mean(self, bessel_correction=False) -> float:
         """
@@ -67,32 +67,32 @@ class DataSetWithStatistics(DataSet):
         Compute the arithmetic mean of a sorted dataset.
         """
 
-        if self.is_number_of_items_even() is False:
+        if self.is_num_of_items_even() is False:
             return self[self.size // 2]
 
-        index_of_second_middle_value = self.size // 2
-        index_of_first_middle_value = index_of_second_middle_value - 1
+        idx_of_second_mid_value = self.size // 2
+        idx_of_first_mid_value = idx_of_second_mid_value - 1
 
-        return (self[index_of_first_middle_value] + self[index_of_second_middle_value]) / 2
+        return (self[idx_of_first_mid_value] + self[idx_of_second_mid_value]) / 2
 
     def mode(self) -> [int, float]:
-        frequency_dictionary = {}
+        freq_dictionary = {}
         for value in self:
-            frequency_dictionary[value] = frequency_dictionary.get(value, 0) + 1
+            freq_dictionary[value] = freq_dictionary.get(value, 0) + 1
 
         modes_list = []
-        max_frequency = 0
-        for value, value_frequency in frequency_dictionary.items():
-            if value_frequency > max_frequency:
+        max_freq = 0
+        for value, value_freq in freq_dictionary.items():
+            if value_freq > max_freq:
                 modes_list = [value]
-                max_frequency = value_frequency
-            elif value_frequency == max_frequency:
+                max_freq = value_freq
+            elif value_freq == max_freq:
                 modes_list.append(value)
 
-        amount_of_modes = len(modes_list)
-        if max_frequency > 1 and amount_of_modes > 1:
+        num_of_modes = len(modes_list)
+        if max_freq > 1 and num_of_modes > 1:
             return modes_list
-        elif max_frequency > 1 and amount_of_modes == 1:
+        elif max_freq > 1 and num_of_modes == 1:
             return modes_list[0]
         else:
             raise ValueError('No mode found')
@@ -100,14 +100,14 @@ class DataSetWithStatistics(DataSet):
     def range(self) -> [int, float]:
         return max(self) - min(self)
 
-    def difference_between_value_and_mean(self, squared=True) -> 'DataSetWithStatistics':
+    def diff_between_value_and_mean(self, squared=True) -> 'DataSetWithStatistics':
         data_set_mean = self.arithmetic_mean()
-        differences = DataSetWithStatistics([value - data_set_mean for value in self])
+        diff = DataSetWithStatistics([value - data_set_mean for value in self])
 
-        return DataSetWithStatistics(differences ** 2 if squared else differences)
+        return DataSetWithStatistics(diff ** 2 if squared else diff)
 
     def variance(self, bessel_correction=True):
-        squared_differences = self.difference_between_value_and_mean()
+        squared_differences = self.diff_between_value_and_mean()
 
         return squared_differences.arithmetic_mean(bessel_correction)
 
@@ -115,29 +115,29 @@ class DataSetWithStatistics(DataSet):
         return sqrt(self.variance(bessel_correction))
 
     def covariance(self, other, bessel_correction=True, total_covariance_only=False):
-        set_1_differences = self.difference_between_value_and_mean(squared=False)
-        set_2_differences = other.difference_between_value_and_mean(squared=False)
-        products_of_sets = set_1_differences * set_2_differences
+        set_1_diff = self.diff_between_value_and_mean(squared=False)
+        set_2_diff = other.diff_between_value_and_mean(squared=False)
+        products_of_sets = set_1_diff * set_2_diff
 
         return sum(products_of_sets) if total_covariance_only \
             else DataSetWithStatistics(products_of_sets).arithmetic_mean(bessel_correction)
 
     def pearson_correlation_coefficient(self, other):
         total_covariance = self.covariance(other, total_covariance_only=True)
-        set_1_sum_of_squared_differences = sum(self.difference_between_value_and_mean(squared=True))
-        set_2_sum_of_squared_differences = sum(other.difference_between_value_and_mean(squared=True))
-        product_of_roots = sqrt(set_1_sum_of_squared_differences) * sqrt(set_2_sum_of_squared_differences)
+        set_1_sum_of_squared_diffs = sum(self.diff_between_value_and_mean(squared=True))
+        set_2_sum_of_squared_diffs = sum(other.diff_between_value_and_mean(squared=True))
+        product_of_roots = sqrt(set_1_sum_of_squared_diffs) * sqrt(set_2_sum_of_squared_diffs)
 
         return total_covariance / product_of_roots
 
     def quantiles(self) -> list:
         sorted_data_set = DataSetWithStatistics(sorted(self))
 
-        index_of_middle_value = sorted_data_set.size // 2
-        first_quartile = DataSetWithStatistics(sorted_data_set[:index_of_middle_value]).median()
+        idx_of_mid_value = sorted_data_set.size // 2
+        first_quartile = DataSetWithStatistics(sorted_data_set[:idx_of_mid_value]).median()
 
-        index_of_middle_value_for_third_quartile = index_of_middle_value if self.is_number_of_items_even() else index_of_middle_value + 1
-        third_quartile = DataSetWithStatistics(sorted_data_set[index_of_middle_value_for_third_quartile:]).median()
+        idx_of_mid_value_for_third_quartile = idx_of_mid_value if self.is_num_of_items_even() else idx_of_mid_value + 1
+        third_quartile = DataSetWithStatistics(sorted_data_set[idx_of_mid_value_for_third_quartile:]).median()
 
         return [first_quartile, self.median(), third_quartile]
 
@@ -146,7 +146,7 @@ class DataSetWithStatistics(DataSet):
 
         return quantiles[2] - quantiles[0]
 
-    def remove_outliers_iqr(self, outlier_step=1.5):
+    def remove_outliers_iqr(self, outlier_step=1.5, log=False):
         quantiles = self.quantiles()
         first_quantile = quantiles[0]
         third_quantile = quantiles[2]
@@ -157,4 +157,5 @@ class DataSetWithStatistics(DataSet):
         for value in reversed(self):
             if value < lower_limit or value > upper_limit:
                 self.remove(value)
-                print(f'The outlier {value} has been removed')
+                if log:
+                    print(f'The outlier {value} has been removed')
